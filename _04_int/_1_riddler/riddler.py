e33f26c4b393768c27478fa9b1f24a3f40129bf4
league <k>
2024-01-26 17:54:48 -0800

    shape selector and riddler.py

--- _04_int/_1_riddler/riddler.py
+++ _04_int/_1_riddler/riddler.py
@@ -1,6 +1,9 @@
+import turtle
+from tkinter import messagebox, simpledialog, Tk
+
 """
 * Write a python program that asks the user a minimum of 3 riddles.
-simpledialog.askstring()
+simpledialog.askstring(title="What do you call a chic) 
 * You can look at riddles.com if you don't already know any riddles.
 
 * Collect the response of each riddle from the user and compare their
@@ -9,5 +12,11 @@ simpledialog.askstring()
 * Use a variable to keep track of the correctly answered riddles
 
 * After all the riddles have been asked, tell the user how many they got
-  correct
+  correct!
 """
+
+user = simpledialog.askstring(title="user", prompt="What do you call a chicken with a bad sunburn?")
+if user =="Fried Chicken!":
+    messagebox.showinfo("How did u get dat right? Yer a pro at dis!")
+else:
+    messagebox.showinfo("Wrong!")
